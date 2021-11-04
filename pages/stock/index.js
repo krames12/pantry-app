d934f6ba880f02cdd4f6f839b0a6d5ee9f79c29a
Nick Kramer <krames12@gmail.com>
2021-11-04 06:55:28 -0400

    successfully gets dat from db

--- pages/stock/index.js
+++ pages/stock/index.js
@@ -1,7 +1,47 @@
 import Stock from "../../components/Stock/Stock"
 
-export default function stockPage() {
+const categoryItemsQuery = `
+  query CategoryItemsQuery {
+    categories {
+      name
+      id
+      category_items {
+        item {
+          id
+          name
+          in_stock
+          restock_soon
+          unit
+        }
+      }
+    }
+  }
+`;
+
+export async function getServerSideProps(context) {
+  console.log(process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT)
+  const res = await fetch(process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_GRAPHQL_SECRET
+    },
+    body: JSON.stringify({
+      "query": categoryItemsQuery,
+      "variables": {}
+    })
+  }).then( response => response.json()).then( ({data}) => data.categories);
+
+  return {
+    props: {
+      categories: res
+    }
+  };
+
+}
+
+export default function stockPage({categories}) {
   return (
-    <Stock />
+    <Stock categories={categories}/>
   )
 }
