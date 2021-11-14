0450da295fca883f63c032e06683a406cab21a30
Nick Kramer <krames12@gmail.com>
2021-11-14 06:11:32 -0500

    changed title from generated

--- components/ItemList/ItemList.js
+++ components/ItemList/ItemList.js
@@ -1,34 +1,38 @@
+import { useState, useEffect } from "react";
 import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
   TableCaption
 } from "@chakra-ui/react";
 
-const ItemList = ({items}) => (
-  <Table variant="simple">
-    <TableCaption>A list of currently available items in your pantry</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>Name</Th>
-        <Th># Available</Th>
-        <Th>Re-stock soon?</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      {items?.map( ({item: {id, name, in_stock, unit, restock_soon}}) => (
-        <Tr key={id}>
-          <Td>{name}</Td>
-          <Td>{in_stock} - {unit}</Td>
-          <Td>{`${restock_soon ? "Restock!" : "You can wait"}`}</Td>
+const ItemList = ({items}) => {
+
+
+  return (
+    <Table variant="simple">
+      <TableCaption>A list of currently available items in your pantry</TableCaption>
+      <Thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th># Available</Th>
+          <Th>Re-stock soon?</Th>
         </Tr>
-      ))}
-    </Tbody>
-  </Table>
-);
+      </Thead>
+      <Tbody>
+        {items?.map( ({item: {id, name, in_stock, unit, restock_soon}}) => (
+          <Tr key={id}>
+            <Td>{name}</Td>
+            <Td>{in_stock} - {unit}</Td>
+            <Td>{`${restock_soon ? "Restock!" : "You can wait"}`}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  )
+};
 
 export default ItemList;
